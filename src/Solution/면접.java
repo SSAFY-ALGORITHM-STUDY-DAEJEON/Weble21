94ac5061411ec8bacb97e7e164cea24f9d43ed3c
김동건 <kdk4270@naver.com>
2026-09-08 14:19:44 +0900

    면접 주석 추가

--- src/Solution/면접.java
+++ src/Solution/면접.java
@@ -6,7 +6,7 @@ import java.util.*;
 public class 면접 {
 	static int score, cnt;
 	
-	
+	// 배열따라 계산하기 - false면 문제 맞음, true면 틀림 -> cnt 초기화
 	private static void calculate(boolean[] arr, int k) {
 		for(int i = 0; i < arr.length; i++) {
 			if(!arr[i]) {
@@ -36,10 +36,14 @@ public class 면접 {
 			
 			score = 0;
 			cnt = 0;
-
+			// n과 m이 같으면 계산 필요 없음
 			if(n != m) {
+				// 틀릴수있는 갯수
 				int wrong = n - m;
+				// 넣을 수 있는 갯수
 				int can = n / k;
+				
+				// 우선 배열 안에 k의 배수들은 다 틀리게 만들기
 				int tmp = wrong;
 				for (int i = n - 1; i >= 0; i--) {
 					if(tmp == 0) break;
@@ -51,6 +55,7 @@ public class 면접 {
 						arr[i] = false;
 					}
 				}
+				// 추가로 더 틀릴 수 있는게 남았다면 추가로 true 만들기
 				int tmp2 = wrong - can;
 				if(tmp2 > 0) {
 					for(int i = 0; i< n; i++) {
