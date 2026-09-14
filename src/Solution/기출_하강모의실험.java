d5c575f75f6714f322dfd4c10a6d2c1e47a7ea57
김동건 <kdk4270@naver.com>
2026-09-14 17:50:11 +0900

    하강모의실험

--- src/Solution/기출_하강모의실험.java
+++ src/Solution/기출_하강모의실험.java
@@ -4,7 +4,72 @@ import java.io.*;
 import java.util.*;
 
 public class 기출_하강모의실험 {
-	public static void main(String[] args) {
+	static int n;
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		n = Integer.parseInt(bf.readLine());
+		int[][] arr = new int[n][n];
+		for (int i = 0; i < n; i++) {
+			StringTokenizer st = new StringTokenizer(bf.readLine());
+			for (int j = 0; j < n; j++) {
+				arr[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+		int tmp = down(arr);
+		System.out.println(tmp);
+
+	}
+
+	private static int down(int[][] arr) {
+		int cur = 0;
+		int result = 0;
+		for (int i = 0; i < n; i++) {
+			int power = arr[cur][i];
+			if(power == 0) {
+				continue;
+			}
+			int k = 1;
+			while(true) {
+				if(arr[cur+k][i] == 1) {
+					cur = cur+k;
+					power++;
+				} else {
+					break;
+				}
+			}
+			//초기 값 계산 끝
+			for(int j = cur + 1; j < n; j++) {
+				if(arr[j][i] == 0) {
+					power *= 1.9;
+				} else {
+					// 맞닿은 곳 무게 몇인지 계산
+					int tmpPower = 0;
+					k = 1;
+					while(true) {
+						if(arr[j+k][i] == 1) {
+							k++;
+							tmpPower++;
+						} else {
+							break;
+						}
+					}
+					// 현재 힘이 더 크면
+					if(tmpPower < power) {
+						power = power + tmpPower;
+						cur = cur + k;
+					} else {
+						// 다음이 더 크면
+						break;
+					}
+				}
+				if(j == n - 1) {
+					result++;
+				}
+			}
+			
+		}
+		return result;
+
 	}
 }
